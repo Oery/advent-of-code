4c0b795f7d1e78e388fadb86559aa343de466cb1
Oery <oery.dev@gmail.com>
2024-12-02 16:48:52 +0100

    Remove Global input loader

--- 2024/Rust/src/day_one.rs
+++ 2024/Rust/src/day_one.rs
@@ -1,4 +1,24 @@
-use crate::utils::load_inputs;
+use std::io::Read;
+
+fn load_inputs() -> (Vec<u32>, Vec<u32>) {
+    let mut input = String::new();
+
+    let mut file = std::fs::File::open("./inputs/day_one.txt").unwrap();
+    file.read_to_string(&mut input).unwrap();
+
+    let mut list_one: Vec<u32> = vec![];
+    let mut list_two: Vec<u32> = vec![];
+
+    for line in input.lines() {
+        let mut line = line.split_whitespace();
+        let first = line.next().unwrap().parse::<u32>().unwrap();
+        let second = line.next().unwrap().parse::<u32>().unwrap();
+        list_one.push(first);
+        list_two.push(second);
+    }
+
+    (list_one, list_two)
+}
 
 fn calculate_total_distance(mut list_one: Vec<u32>, mut list_two: Vec<u32>) -> u32 {
     list_one.sort();
@@ -20,12 +40,12 @@ fn calculate_total_similarity(list_one: Vec<u32>, list_two: Vec<u32>) -> u32 {
 
 pub fn day_one() {
     // Day 1 - Part 1
-    let (list_one, list_two) = load_inputs("day_one");
+    let (list_one, list_two) = load_inputs();
     let total = calculate_total_distance(list_one, list_two);
     println!("The Total Distance is {total}");
 
     // Day 1 - Part 2
-    let (list_one, list_two) = load_inputs("day_one");
+    let (list_one, list_two) = load_inputs();
     let similarity = calculate_total_similarity(list_one, list_two);
     println!("The Total Similarity is {similarity}");
 }
